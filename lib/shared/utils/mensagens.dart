1ed7b9c6b2c90a0c3b85a1cac3cf7c61fd71f442
Richard King <richard.kingjunior1@gmail.com>
2022-08-10 23:31:12 -0300

    feature(notificações): adiciona notificações

---
+++ lib/shared/utils/mensagens.dart
@@ -0,0 +1,24 @@
+class Mensagens {
+
+  List<String> titulos = [
+    'Oi, é o JJC.',
+    'Oi, é o JJC.',
+    'Oi, é o JJC.',
+    'Oi, é o JJC.',
+    'Oi, é o JJC.',
+    'Oi, é o JJC.',
+    'Oi, é o JJC.',
+  ];
+
+  List<String> notificacoes = [
+    'Parece que você não reviu suas técnicas ontem, venha treinar.',
+    'Faça seu tempo valer. Aulas de arm-lock agora!',
+    'Não desista do seu grau, venha estudar',
+    'Vai continuar sendo amassado? Aprenda a vencer seus rolas!',
+    'Você não vai aprender berimbolo assistindo dancinha.',
+    'Não deixe suas tecnicas enferrujar.',
+    'O faixa branca ta vindo te finalizar.'
+  ];
+
+  String despedida  = 'Parece que esses alertas não estão funcionando. Vamos parar de envia-los por enquanto =(';
+}
